78abed5cd350d9568e2b79f1474b631457f9812c
ly <352248499@qq.com>
2017-09-01 13:41:50 +0800

    哈哈哈

--- src/com/youedata/test/Asd.java
+++ src/com/youedata/test/Asd.java
@@ -1,5 +1,7 @@
 package com.youedata.test;
 
 public class Asd {
-
+	public static void main(String[] args) {
+		System.out.println("asd");
+	}
 }
--- src/com/youedata/test/HaHa.java
+++ src/com/youedata/test/HaHa.java
@@ -1,5 +1,8 @@
 package com.youedata.test;
 
 public class HaHa {
+	public static void main(String[] args) {
+		System.out.println("haha");
+	}
 
 }
